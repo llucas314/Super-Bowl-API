34b3294156c93cbe13df2154e8094f254eff9f9e
Ren Lucas <llucas314@gmail.com>
2019-11-14 16:54:05 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const parser = require("body-parser");
-const router = require("./routes/index");
+const router = require("./lib/routes/index");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(parser.json());
 app.use(router);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
