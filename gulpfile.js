534127d9eb3327ba7a16105446be9ff60aff2548
Ren Lucas <llucas314@gmail.com>
2019-11-14 19:57:36 -0500

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const yaml = require("js-yaml");
+const path = require("path");
+const fs = require("fs");
+
+gulp.task("swagger", function() {
+  let doc = yaml.safeLoad(
+    fs.readFileSync(path.join(__dirname, "./api/swagger/swagger.yaml"))
+  );
+  fs.writeFileSync(
+    path.join(__dirname, "./public/swagger.json"),
+    JSON.stringify(doc, null, " ")
+  );
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./api/swagger/swagger.yaml", ["swagger"]);
+});
